356b25a69463d8b31eda7385eafc75e8924ace01
Karol Sobczak <ksobczak@hadapt.com>
2015-04-01 16:18:29 +0200

    Add presto product tests subproject in presto git repository
    Summary:
    An initial draft of presto product tests subproject.
    Actual tests to be added.
    A mechanism for hive5 jdbc driver fat jar support to be added
    
    Task: SWARM-143
    Review Url: @@review_url@@
    
    Test Plan: manual tests
    
    Reviewers: losipiuk, anton, sogorkis
    
    Subscribers: pnowojski
    
    Differential Revision: https://phabricator.td.teradata.com/D118

---
+++ presto-product-tests/src/test/java/com/facebook/presto/tests/SimpleQueryTest.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.tests;
+
+import com.teradata.test.AfterTestWithContext;
+import com.teradata.test.BeforeTestWithContext;
+import com.teradata.test.ProductTest;
+import com.teradata.test.Requirement;
+import com.teradata.test.RequirementsProvider;
+import com.teradata.test.Requires;
+import com.teradata.test.assertions.QueryAssert;
+import com.teradata.test.fulfillment.hive.ImmutableHiveTableRequirement;
+import org.testng.annotations.Test;
+
+import static com.teradata.test.assertions.QueryAssert.Row.row;
+import static com.teradata.test.context.ThreadLocalTestContextHolder.testContextIfSet;
+import static com.teradata.test.fulfillment.hive.tpch.TpchTableDefinitions.NATION;
+import static com.teradata.test.query.QueryExecutor.query;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimpleQueryTest
+        extends ProductTest
+{
+    private static class SimpleTestRequirements
+            implements RequirementsProvider
+    {
+        @Override
+        public Requirement getRequirements()
+        {
+            return new ImmutableHiveTableRequirement(NATION);
+        }
+    }
+
+    @BeforeTestWithContext
+    public void beforeTest()
+    {
+        assertThat(testContextIfSet().isPresent()).isTrue();
+    }
+
+    @AfterTestWithContext
+    public void afterTest()
+    {
+        assertThat(testContextIfSet().isPresent()).isTrue();
+    }
+
+    @Test(groups = "create_as_select")
+    @Requires(SimpleTestRequirements.class)
+    public void selectAllFromNation()
+    {
+        QueryAssert.assertThat(query("select * from nation")).hasRowsCount(25);
+    }
+
+    @Test(groups = {"create_as_select"})
+    @Requires(SimpleTestRequirements.class)
+    public void selectCountFromNation()
+    {
+        QueryAssert.assertThat(query("select count(*) from nation"))
+                .hasRowsCount(1)
+                .hasRows(row(25));
+    }
+
+    @Test(groups = "failing")
+    public void failingTest()
+    {
+        assertThat(1).isEqualTo(2);
+    }
+
+    @Test(groups = "skipped", enabled = false)
+    public void disabledTest()
+    {
+        assertThat(1).isEqualTo(2);
+    }
+}
