dffc1de988fc9cd175ab0ed8ac2d91ab6ae18142
Christina Wallin <cwallin@hadapt.com>
2015-05-29 17:05:48 -0400

    Docs: Update download links for 141t

--- presto-docs/src/main/sphinx/ext/download.py
+++ presto-docs/src/main/sphinx/ext/download.py
@@ -17,41 +17,56 @@ from docutils import nodes, utils
 # noinspection PyUnresolvedReferences
 from sphinx.errors import SphinxError
 
+TD_LINK_SERVER = 'http://www.teradata.com/presto'
+TD_LINK_CLI = 'http://www.teradata.com/presto'
+TD_LINK_JDBC = 'http://www.teradata.com/presto'
+TD_LINK_DEMO_CDH = 'http://www.teradata.com/presto'
+TD_LINK_DEMO_HDP = 'http://www.teradata.com/presto'
+TD_LINK_PRESTO_DOCS = 'http://www.teradata.com/presto'
+TD_LINK_PRESTO_YARN_PACKAGE = 'http://www.teradata.com/presto'
+TD_LINK_PRESTO_AMBARI_PLUGIN = 'http://www.teradata.com/presto'
+TD_LINK_SLIDER = 'http://archive.apache.org/dist/incubator/slider/0.80.0-incubating/slider-assembly-0.80.0-incubating-all.tar.gz'
+TD_LINK_ODBC_MAC = 'http://www.teradata.com/presto'
+TD_LINK_ODBC_WIN64 = 'http://www.teradata.com/presto'
+TD_LINK_ODBC_WIN32 = 'http://www.teradata.com/presto'
+TD_LINK_ODBC_LINUX64 = 'http://www.teradata.com/presto'
+TD_LINK_ODBC_LINUX32 = 'http://www.teradata.com/presto'
+TD_LINK_ODBC_DOCS = 'http://www.teradata.com/presto'
+
 GROUP_ID = 'com.facebook.presto'
 ARTIFACTS = {
-    'server': ('presto-server', 'tar.gz', None),
-    'cli': ('presto-cli', 'jar', 'executable'),
-    'jdbc': ('presto-jdbc', 'jar', None),
-    'verifier': ('presto-verifier', 'jar', 'executable'),
-    'benchmark-driver': ('presto-benchmark-driver', 'jar', 'executable'),
+    'server': ('presto-server-rpm-0.141t', 'rpm', TD_LINK_SERVER),
+    'server-old': ('presto-server-rpm4.6-0.141t', 'rpm', TD_LINK_SERVER),
+    'cli': ('presto-cli-0.141t', 'jar', TD_LINK_CLI),
+    'jdbc': ('presto-jdbc-0.141t', 'jar', TD_LINK_JDBC),
+    'demo-cdh': ('presto-demo-cdh-0.141t', 'ova', TD_LINK_DEMO_CDH),
+    'demo-hdp': ('presto-demo-hdp-0.141t', 'ova', TD_LINK_DEMO_HDP),
+    'presto-yarn-package': ('presto-yarn-package-1.0.1-0.141t', 'zip', TD_LINK_PRESTO_YARN_PACKAGE),
+    'presto-ambari-plugin': ('ambari-presto-1.0', 'tar.gz', TD_LINK_PRESTO_DOCS),
+    'presto-admin': ('prestoadmin-1.2', 'tar.bz2', TD_LINK_PRESTO_DOCS),
+    'apache-slider': ('slider-assembly-0.80.0-incubating-all', 'tar.gz', TD_LINK_PRESTO_DOCS),
+    'presto-docs': ('presto-docs-0.141t-download', 'zip', TD_LINK_PRESTO_DOCS),
+
+    'odbc-mac': ('TeradataPrestoODBC-1.0.0.1001', 'dmg', TD_LINK_ODBC_MAC),
+    'odbc-windows-64': ('TeradataPrestoODBC64-1.0.0.1001', 'msi', TD_LINK_ODBC_WIN64),
+    'odbc-windows-32': ('TeradataPrestoODBC32-1.0.0.1001', 'msi', TD_LINK_ODBC_WIN32),
+    'odbc-linux-64': ('TeradataPrestoODBC-64bit-1.0.0.1001', 'rpm', TD_LINK_ODBC_LINUX64,),
+    'odbc-linux-32': ('TeradataPrestoODBC-32bit-1.0.0.1001', 'rpm', TD_LINK_ODBC_LINUX32),
+    'odbc-documentation': ('Teradata ODBC Driver for Presto Install Guide', 'pdf', TD_LINK_ODBC_DOCS)
 }
-
-
-def maven_filename(artifact, version, packaging, classifier):
-    classifier = '-' + classifier if classifier else ''
-    return '%s-%s%s.%s' % (artifact, version, classifier, packaging)
-
-
-def maven_download(group, artifact, version, packaging, classifier):
-    base = 'https://repo1.maven.org/maven2/'
-    group_path = group.replace('.', '/')
-    filename = maven_filename(artifact, version, packaging, classifier)
-    return base + '/'.join((group_path, artifact, version, filename))
-
 
 def setup(app):
     # noinspection PyDefaultArgument,PyUnusedLocal
     def download_link_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
-        version = app.config.release
 
         if not text in ARTIFACTS:
             inliner.reporter.error('Unsupported download type: ' + text)
             return [], []
 
-        artifact, packaging, classifier = ARTIFACTS[text]
+        artifact, packaging, uri = ARTIFACTS[text]
 
-        title = maven_filename(artifact, version, packaging, classifier)
-        uri = maven_download(GROUP_ID, artifact, version, packaging, classifier)
+        title = artifact + '.' + packaging
+        uri = uri
 
         node = nodes.reference(title, title, internal=False, refuri=uri)
 
