48a53f8e314cf10242862ca39ac8f1e2461c3f02
Alec Thompson <alec.thompson@teradata.com>
2015-07-14 13:26:17 -0400

    Added Brian's work on postgres tests

---
+++ presto-product-tests/src/test/resources/sql-tests/testcases/connectors/postgres/describe_table.sql
@@ -0,0 +1,11 @@
+-- database: presto; groups: postgres_connector; queryType: SELECT
+-- delimiter: |; ignoreOrder: true;
+--!
+describe postgres.public.datatype
+--!
+c_bigint|bigint|true|false||
+c_double|double|true|false||
+c_varchar|varchar|true|false||
+c_date|date|true|false||
+c_timestamp|timestamp|true|false||
+c_boolean|boolean|true|false||
