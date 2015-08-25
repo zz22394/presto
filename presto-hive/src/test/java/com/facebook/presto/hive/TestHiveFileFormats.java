281625ee60a14a215fc260e73ec154511ccce9a2
Stanislaw Ogorkis <stanislaw.ogorkis@teradata.com>
2015-08-25 14:14:08 +0200

    Support for TIMESTAMP type in Parquet format
    This commit adds support for TIMESTAMP type in Parquet file format. Parquet TIMESTAMPS
    are stored as two components: milliseconds of day (8 bytes) and julian date (4 bytes). It
    is stored in GMT timezone.

--- presto-hive/src/test/java/com/facebook/presto/hive/TestHiveFileFormats.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestHiveFileFormats.java
@@ -253,9 +253,9 @@ public class TestHiveFileFormats
                     return false;
                 }
 
-                // Parquet does not support DATE, TIMESTAMP, or BINARY
+                // Parquet does not support DATE, or BINARY
                 ObjectInspector objectInspector = testColumn.getObjectInspector();
-                return !hasType(objectInspector, PrimitiveCategory.DATE, PrimitiveCategory.TIMESTAMP, PrimitiveCategory.BINARY);
+                return !hasType(objectInspector, PrimitiveCategory.DATE, PrimitiveCategory.BINARY);
             }
         }));
 
