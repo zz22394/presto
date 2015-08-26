d53f169d14d52981e9eb54f2b1b2625b97b67b66
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2015-08-26 18:59:17 +0200

    exit with status 1 when jar download fails

--- presto-product-tests/libs/download.sh
+++ presto-product-tests/libs/download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e;
 
 HIVE_JDBC_DOWNLOAD_URL='http://bdch-ftp.td.teradata.com:8081/nexus/service/local/artifact/maven/redirect?r=test-framework&g=org.apache.hive&a=hive-jdbc-all&v=1.0-SNAPSHOT&e=jar'
 HIVE_JDBC_JAR_NAME='test-framework-hive-jdbc-all.jar'
@@ -11,6 +12,7 @@ if [[ (! -f ${HIVE_JDBC_JAR_NAME}) || (! -s ${HIVE_JDBC_JAR_NAME})  ]]; then
     if [ $? -ne 0 ]; then
         echo "Download ${HIVE_JDBC_JAR_NAME} failed."
         rm -rf ${HIVE_JDBC_JAR_NAME}
+        exit 1
     else
         echo "${HIVE_JDBC_JAR_NAME} has been successfully downloaded."
     fi
