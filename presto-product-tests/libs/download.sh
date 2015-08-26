3eafc29d49ec7f243509812f0cbfceb3f34d8b3c
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2015-08-26 16:37:49 +0200

    presto-product-tests: download hive-jdbc-fat.jar only if it is needed
    Hive jdbc jar was dowloaded for each presto build, even when productTest profile was not activated.
    Also download.sh was not checking whether the jar download had been finished succesfully.
    
    Testing: manual

--- presto-product-tests/libs/download.sh
+++ presto-product-tests/libs/download.sh
@@ -3,8 +3,17 @@
 HIVE_JDBC_DOWNLOAD_URL='http://bdch-ftp.td.teradata.com:8081/nexus/service/local/artifact/maven/redirect?r=test-framework&g=org.apache.hive&a=hive-jdbc-all&v=1.0-SNAPSHOT&e=jar'
 HIVE_JDBC_JAR_NAME='test-framework-hive-jdbc-all.jar'
 
-if [ ! -f ${HIVE_JDBC_JAR_NAME} ]; then
+# when jar doesn't exist yet, or the jar is just empty file (previous download was failed)
+if [[ (! -f ${HIVE_JDBC_JAR_NAME}) || (! -s ${HIVE_JDBC_JAR_NAME})  ]]; then
+    rm -rf ${HIVE_JDBC_JAR_NAME}
+    echo "Downloading ${HIVE_JDBC_JAR_NAME} ..."
     wget ${HIVE_JDBC_DOWNLOAD_URL} -O ${HIVE_JDBC_JAR_NAME}
+    if [ $? -ne 0 ]; then
+        echo "Download ${HIVE_JDBC_JAR_NAME} failed."
+        rm -rf ${HIVE_JDBC_JAR_NAME}
+    else
+        echo "${HIVE_JDBC_JAR_NAME} has been successfully downloaded."
+    fi
 else
     echo "File ${HIVE_JDBC_JAR_NAME} already downloaded"
 fi
