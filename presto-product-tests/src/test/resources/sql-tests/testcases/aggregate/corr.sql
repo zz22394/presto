2652e94e7b3388c82cfaadc21441ce0b2b1814fa
Anu Sudarsan <anu.sudarsan@teradata.com>
2015-09-03 16:31:29 -0400

    Fix group name in tests

--- presto-product-tests/src/test/resources/sql-tests/testcases/aggregate/corr.sql
+++ presto-product-tests/src/test/resources/sql-tests/testcases/aggregate/corr.sql
@@ -1,2 +1,2 @@
--- database: presto; groups: mytest; tables: orders
+-- database: presto; groups: aggregate; tables: orders
 select round(corr(o_totalprice, o_orderkey),6) from orders
--- presto-product-tests/src/test/resources/sql-tests/testcases/aggregate/regr.sql
+++ presto-product-tests/src/test/resources/sql-tests/testcases/aggregate/regr.sql
@@ -1,2 +1,2 @@
--- database: presto; groups: mytest; tables: orders
+-- database: presto; groups: aggregate; tables: orders
 select round(regr_slope(o_totalprice, o_custkey), 4) , round(regr_intercept(o_totalprice, o_orderkey), 4) from orders
