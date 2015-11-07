66bc4f177101c955b413e5320009120604fe6538
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2015-11-07 17:52:01 +0100

    presto-blackhole: add page processing delay
    "page_processing_delay" blackhole table property added.
    With this property delay between pages processing can be specified.

--- presto-blackhole/src/main/java/com/facebook/presto/plugin/blackhole/BlackHoleConnector.java
+++ presto-blackhole/src/main/java/com/facebook/presto/plugin/blackhole/BlackHoleConnector.java
@@ -26,11 +26,14 @@ import com.facebook.presto.spi.transaction.IsolationLevel;
 import com.facebook.presto.spi.type.TypeManager;
 import com.facebook.presto.spi.type.TypeSignatureParameter;
 import com.google.common.collect.ImmutableList;
+import io.airlift.units.Duration;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 import static com.facebook.presto.spi.session.PropertyMetadata.integerSessionProperty;
 import static com.facebook.presto.spi.type.StandardTypes.ARRAY;
+import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
 import static com.facebook.presto.spi.type.VarcharType.createUnboundedVarcharType;
 import static java.util.Locale.ENGLISH;
 import static java.util.stream.Collectors.toList;
@@ -43,6 +46,7 @@ public class BlackHoleConnector
     public static final String ROWS_PER_PAGE_PROPERTY = "rows_per_page";
     public static final String FIELD_LENGTH_PROPERTY = "field_length";
     public static final String DISTRIBUTED_ON = "distributed_on";
+    public static final String PAGE_PROCESSING_DELAY = "page_processing_delay";
 
     private final BlackHoleMetadata metadata;
     private final BlackHoleSplitManager splitManager;
@@ -50,13 +54,16 @@ public class BlackHoleConnector
     private final BlackHolePageSinkProvider pageSinkProvider;
     private final BlackHoleNodePartitioningProvider partitioningProvider;
     private final TypeManager typeManager;
+    private final ExecutorService executorService;
 
-    public BlackHoleConnector(BlackHoleMetadata metadata,
+    public BlackHoleConnector(
+            BlackHoleMetadata metadata,
             BlackHoleSplitManager splitManager,
             BlackHolePageSourceProvider pageSourceProvider,
             BlackHolePageSinkProvider pageSinkProvider,
             BlackHoleNodePartitioningProvider partitioningProvider,
-            TypeManager typeManager)
+            TypeManager typeManager,
+            ExecutorService executorService)
     {
         this.metadata = metadata;
         this.splitManager = splitManager;
@@ -64,6 +71,7 @@ public class BlackHoleConnector
         this.pageSinkProvider = pageSinkProvider;
         this.partitioningProvider = partitioningProvider;
         this.typeManager = typeManager;
+        this.executorService = executorService;
     }
 
     @Override
@@ -137,12 +145,27 @@ public class BlackHoleConnector
                         value -> ImmutableList.copyOf(((List<String>) value).stream()
                                 .map(name -> name.toLowerCase(ENGLISH))
                                 .collect(toList())),
-                        List.class::cast));
+                        List.class::cast),
+                new PropertyMetadata<>(
+                        PAGE_PROCESSING_DELAY,
+                        "Sleep duration before processing each page",
+                        VARCHAR,
+                        Duration.class,
+                        Duration.valueOf("0s"),
+                        false,
+                        value -> Duration.valueOf((String) value),
+                        Duration::toString));
     }
 
     @Override
     public ConnectorNodePartitioningProvider getNodePartitioningProvider()
     {
         return partitioningProvider;
+    }
+
+    @Override
+    public void shutdown()
+    {
+        executorService.shutdownNow();
     }
 }
