70f46348785ffe22a86c46b87a0475fdcce6cb3d
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2016-01-25 17:00:13 -0500

    Implement EXECUTE prepared statements
    Syntax:
    EXECUTE <name>
    
    Executes the prepared statement named name. Does not yet support
    paramters.
    
    Testing: added unit tests, tested via cli

--- presto-parser/src/main/java/com/facebook/presto/sql/tree/AstVisitor.java
+++ presto-parser/src/main/java/com/facebook/presto/sql/tree/AstVisitor.java
@@ -97,6 +97,11 @@ public abstract class AstVisitor<R, C>
         return visitDataDefinitionStatement(node, context);
     }
 
+    protected R visitExecute(Execute node, C context)
+    {
+        return visitStatement(node, context);
+    }
+
     protected R visitQuery(Query node, C context)
     {
         return visitStatement(node, context);
