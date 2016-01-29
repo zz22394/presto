f9c2684b7adbe804e1bcf25bd107dd5537ef0586
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2016-01-29 10:46:50 -0500

    Add support for parameters
    Allow ?'s in prepared statements to represent parameters.  Add error handling
    for invalid use of parameters. Rewrite the statement from an execute query to
    replace any ?'s with the parameters supplied in the USING clause.
    
    SYNTAX:
    Parameters can be used within prepared statements and are represented by
    question marks.  e.g.:
    PREPARE my_query FROM SELECT * FROM foo WHERE col1 = ? AND col2 < ?
    
    To add parameters to execute, you use a using clause:
    EXECUTE my_query USING 'abc', 123
    
    Testing: added unit and AbstractQueries tests

--- presto-parser/src/main/java/com/facebook/presto/sql/parser/AstBuilder.java
+++ presto-parser/src/main/java/com/facebook/presto/sql/parser/AstBuilder.java
@@ -75,6 +75,7 @@ import com.facebook.presto.sql.tree.JoinOn;
 import com.facebook.presto.sql.tree.JoinUsing;
 import com.facebook.presto.sql.tree.LambdaExpression;
 import com.facebook.presto.sql.tree.LikePredicate;
+import com.facebook.presto.sql.tree.Literal;
 import com.facebook.presto.sql.tree.LogicalBinaryExpression;
 import com.facebook.presto.sql.tree.LongLiteral;
 import com.facebook.presto.sql.tree.NaturalJoin;
@@ -83,6 +84,7 @@ import com.facebook.presto.sql.tree.NodeLocation;
 import com.facebook.presto.sql.tree.NotExpression;
 import com.facebook.presto.sql.tree.NullIfExpression;
 import com.facebook.presto.sql.tree.NullLiteral;
+import com.facebook.presto.sql.tree.Parameter;
 import com.facebook.presto.sql.tree.Prepare;
 import com.facebook.presto.sql.tree.QualifiedName;
 import com.facebook.presto.sql.tree.QualifiedNameReference;
@@ -142,6 +144,7 @@ import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -156,6 +159,7 @@ class AstBuilder
         extends SqlBaseBaseVisitor<Node>
 {
     private final ParsingOptions parsingOptions;
+    private int parameterPosition = 0;
 
     AstBuilder(ParsingOptions parsingOptions)
     {
@@ -344,7 +348,11 @@ class AstBuilder
     public Node visitExecute(SqlBaseParser.ExecuteContext context)
     {
         String name = context.identifier().getText();
-        return new Execute(getLocation(context), name);
+        List<Literal> parameterValues = new ArrayList<>(context.literal().size());
+        for (SqlBaseParser.LiteralContext literalContext : context.literal()) {
+            parameterValues.add((Literal) visit(literalContext));
+        }
+        return new Execute(getLocation(context), name, parameterValues);
     }
 
     // ********************** query expressions ********************
@@ -1292,6 +1300,12 @@ class AstBuilder
                         .map((x) -> x.getChild(0).getPayload())
                         .map(Token.class::cast)
                         .map(AstBuilder::getIntervalFieldType));
+    }
+
+    @Override
+    public Node visitParameter(SqlBaseParser.ParameterContext context)
+    {
+        return new Parameter(getLocation(context), parameterPosition++);
     }
 
     // ***************** arguments *****************
