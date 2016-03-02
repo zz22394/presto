076592aae43231e8b911a195db2488ae9cfcc339
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-03-02 19:13:11 +0100

    Quarantine Hive CHAR(x) product test

--- presto-product-tests/src/main/resources/sql-tests/testcases/orc/select_char_orc_dictionary.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/orc/select_char_orc_dictionary.sql
@@ -1,4 +1,4 @@
--- database: presto; groups: varchar; tables: orc_char_dictionary;
+-- database: presto; groups: varchar,quarantine; tables: orc_char_dictionary;
 --!
 SELECT c_char FROM orc_char_dictionary WHERE c_char IS NOT NULL LIMIT 1
 --!
