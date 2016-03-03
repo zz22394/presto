df0d37c06b73fb97bbf8089c36fe48d3beb282ab
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-03-03 12:46:19 +0100

    Migrate decsribe_table product tests for mysql/pgsql
    Now only 3 columns are expected.

--- presto-product-tests/src/main/resources/sql-tests/testcases/connectors/mysql/describe_table.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/connectors/mysql/describe_table.sql
@@ -3,11 +3,11 @@
 describe mysql.test.workers_jdbc
 --!
 -- delimiter: |; trimValues: true; ignoreOrder: true;
-id_employee        | bigint  | true | false | |
-first_name         | varchar | true | false | |
-last_name          | varchar | true | false | |
-date_of_employment | date    | true | false | |
-department         | bigint  | true | false | |
-id_department      | bigint  | true | false | |
-name               | varchar | true | false | |
-salary             | bigint  | true | false | |
+id_employee        | bigint  | |
+first_name         | varchar | |
+last_name          | varchar | |
+date_of_employment | date    | |
+department         | bigint  | |
+id_department      | bigint  | |
+name               | varchar | |
+salary             | bigint  | |
