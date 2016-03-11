c90f792faa203a09bdceb52ade96dde866011d4d
Karol Sobczak <Karol.Sobczak@Teradata.com>
2016-03-11 08:44:31 +0100

    Improvements to explain analyze
    Support for:
    - nodes that span across multiple pipelines and operators
    - ScanFilterProject operator
    - waiting for upstream stages to complete

--- presto-tests/src/test/java/com/facebook/presto/tests/TestTpchDistributedQueries.java
+++ presto-tests/src/test/java/com/facebook/presto/tests/TestTpchDistributedQueries.java
@@ -33,8 +33,46 @@ public class TestTpchDistributedQueries
     @Test
     public void testExplainAnalyze()
     {
-        assertExplainAnalyze("EXPLAIN ANALYZE SELECT * FROM orders");
-        assertExplainAnalyze("EXPLAIN ANALYZE SELECT count(*), clerk FROM orders GROUP BY clerk");
+        assertExplainAnalyze("EXPLAIN ANALYZE SELECT * FROM orders",
+                "Fragment 1 \\[SOURCE\\]\n" +
+                        "    Cost: CPU .*, Input: 15000 lines \\(1\\.92MB\\), Output: 15000 lines \\(1\\.92MB\\)\n" +
+                        "    Output layout: \\[orderkey, custkey, orderstatus, totalprice, orderdate, orderpriority, clerk, shippriority, comment\\]\n" +
+                        "    Output partitioning: SINGLE \\[\\]\n" +
+                        "    - TableScan\\[tpch:tpch:orders:sf0\\.01, originalConstraint = true] => \\[orderkey:bigint, custkey:bigint, orderstatus:varchar, totalprice:double, orderdate:date, orderpriority:varchar, clerk:varchar, shippriority:bigint, comment:varchar\\]\n" +
+                        "            Cost: .*%, Output: 15000 lines \\(1\\.92MB\\)\n" +
+                        "            orderkey := tpch:orderkey\n" +
+                        "            custkey := tpch:custkey\n" +
+                        "            orderstatus := tpch:orderstatus\n" +
+                        "            totalprice := tpch:totalprice\n" +
+                        "            orderdate := tpch:orderdate\n" +
+                        "            orderpriority := tpch:orderpriority\n" +
+                        "            clerk := tpch:clerk\n" +
+                        "            shippriority := tpch:shippriority\n" +
+                        "            comment := tpch:comment\n\n");
+
+        assertExplainAnalyze("EXPLAIN ANALYZE SELECT count(*), clerk FROM orders GROUP BY clerk",
+                "Fragment 1 \\[HASH\\]\n" +
+                        "    Cost: CPU .*, Input: 12046 lines \\(447\\.51kB\\), Output: 1000 lines \\(37\\.17kB\\)\n" +
+                        "    Output layout: \\[clerk, \\$hashvalue, count\\]\n" +
+                        "    Output partitioning: SINGLE \\[\\]\n" +
+                        "    - Aggregate\\(FINAL\\)\\[clerk\\] => \\[clerk:varchar, \\$hashvalue:bigint, count:bigint\\]\n" +
+                        "            Cost: .*, Output: 1000 lines \\(37\\.17kB\\)\n" +
+                        "            count := \"count\"\\(\"count_8\"\\)\n" +
+                        "        - RemoteSource\\[2\\] => \\[clerk:varchar, \\$hashvalue:bigint, count_8:bigint\\]\n" +
+                        "                Cost: .*, Output: 12046 lines \\(447\\.51kB\\)\n" +
+                        "\n" +
+                        "Fragment 2 \\[SOURCE\\]\n" +
+                        "    Cost: CPU .*, Input: 15000 lines \\(424\\.92kB\\), Output: 12046 lines \\(447\\.14kB\\)\n" +
+                        "    Output layout: \\[clerk, \\$hashvalue, count_8\\]\n" +
+                        "    Output partitioning: HASH \\[clerk\\]\n" +
+                        "    - Aggregate\\(PARTIAL\\)\\[clerk\\] => \\[clerk:varchar, \\$hashvalue:bigint, count_8:bigint\\]\n" +
+                        "            Cost: .*, Output: 12046 lines \\(447\\.14kB\\)\n" +
+                        "            count_8 := \"count\"\\(\\*\\)\n" +
+                        "        - ScanFilterAndProject\\[table = tpch:tpch:orders:sf0\\.01, originalConstraint = true\\] => \\[clerk:varchar, \\$hashvalue:bigint\\]\n" +
+                        "                Cost: .*, Input: 15000 lines \\(0B\\), Output: 15000 lines \\(424\\.92kB\\), Filtered: 0\\.00%\n" +
+                        "                \\$hashvalue := \"combine_hash\"\\(0, COALESCE\\(\"\\$operator\\$hash_code\"\\(\"clerk\"\\), 0\\)\\)\n" +
+                        "                clerk := tpch:clerk\n\n");
+
         assertExplainAnalyze(
                 "EXPLAIN ANALYZE SELECT x + y FROM (" +
                         "   SELECT orderdate, COUNT(*) x FROM orders GROUP BY orderdate) a JOIN (" +
@@ -58,6 +96,7 @@ public class TestTpchDistributedQueries
                 "    SELECT orderkey\n" +
                 "    FROM lineitem\n" +
                 "    WHERE orderkey % 7 = 0)");
+
         assertExplainAnalyze("EXPLAIN ANALYZE SELECT count(*), clerk FROM orders GROUP BY clerk UNION ALL SELECT sum(orderkey), clerk FROM orders GROUP BY clerk");
     }
 
@@ -72,5 +111,11 @@ public class TestTpchDistributedQueries
         String value = getOnlyElement(computeActual(query).getOnlyColumnAsSet());
         // TODO: check that rendered plan is as expected, once stats are collected in a consistent way
         assertTrue(value.contains("Cost: "), format("Expected output to contain \"Cost: \", but it is %s", value));
+    }
+
+    private void assertExplainAnalyze(@Language("SQL") String query, String expected)
+    {
+        String value = getOnlyElement(computeActual(query).getOnlyColumnAsSet());
+        assertTrue(value.matches(expected), format("Expected output to match %s, but it is %s", expected, value));
     }
 }
