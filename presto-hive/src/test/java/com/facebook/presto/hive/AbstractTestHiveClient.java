0886db6d3cb3399e508e563b0bf15318c65bae2e
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-03-23 15:57:38 +0100

    Make RCFILE readers compatible with Keberos

--- presto-hive/src/test/java/com/facebook/presto/hive/AbstractTestHiveClient.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/AbstractTestHiveClient.java
@@ -1205,7 +1205,7 @@ public abstract class AbstractTestHiveClient
         ConnectorPageSourceProvider pageSourceProvider = new HivePageSourceProvider(
                 new HiveClientConfig().setTimeZone(timeZone.getID()),
                 hdfsEnvironment,
-                ImmutableSet.<HiveRecordCursorProvider>of(new ColumnarTextHiveRecordCursorProvider()),
+                ImmutableSet.<HiveRecordCursorProvider>of(new ColumnarTextHiveRecordCursorProvider(hdfsEnvironment)),
                 ImmutableSet.<HivePageSourceFactory>of(),
                 TYPE_MANAGER);
 
@@ -1239,7 +1239,7 @@ public abstract class AbstractTestHiveClient
         ConnectorPageSourceProvider pageSourceProvider = new HivePageSourceProvider(
                 new HiveClientConfig().setTimeZone(timeZone.getID()),
                 hdfsEnvironment,
-                ImmutableSet.<HiveRecordCursorProvider>of(new ColumnarBinaryHiveRecordCursorProvider()),
+                ImmutableSet.<HiveRecordCursorProvider>of(new ColumnarBinaryHiveRecordCursorProvider(hdfsEnvironment)),
                 ImmutableSet.<HivePageSourceFactory>of(),
                 TYPE_MANAGER);
 
