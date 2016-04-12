96364e362746feb09cf1ad024e8c9d3692d255be
Karol Sobczak <Karol.Sobczak@Teradata.com>
2016-04-12 12:15:14 +0200

    fix airbase version

--- presto-client/src/main/java/com/facebook/presto/client/StatementClient.java
+++ presto-client/src/main/java/com/facebook/presto/client/StatementClient.java
@@ -296,25 +296,25 @@ public class StatementClient
 
     private void processResponse(JsonResponse<QueryResults> response)
     {
-        for (String setSession : response.getHeaders().get(PRESTO_SET_SESSION)) {
+        for (String setSession : response.getHeaders(PRESTO_SET_SESSION)) {
             List<String> keyValue = SESSION_HEADER_SPLITTER.splitToList(setSession);
             if (keyValue.size() != 2) {
                 continue;
             }
             setSessionProperties.put(keyValue.get(0), keyValue.size() > 1 ? keyValue.get(1) : "");
         }
-        for (String clearSession : response.getHeaders().get(PRESTO_CLEAR_SESSION)) {
+        for (String clearSession : response.getHeaders(PRESTO_CLEAR_SESSION)) {
             resetSessionProperties.add(clearSession);
         }
 
-        for (String entry : response.getHeaders().get(PRESTO_ADDED_PREPARE)) {
+        for (String entry : response.getHeaders(PRESTO_ADDED_PREPARE)) {
             List<String> keyValue = SESSION_HEADER_SPLITTER.splitToList(entry);
             if (keyValue.size() != 2) {
                 continue;
             }
             this.addedPreparedStatements.put(keyValue.get(0), keyValue.get(1));
         }
-        for (String entry : response.getHeaders().get(PRESTO_DEALLOCATED_PREPARE)) {
+        for (String entry : response.getHeaders(PRESTO_DEALLOCATED_PREPARE)) {
             this.deallocatedPreparedStatements.add(entry);
         }
 
