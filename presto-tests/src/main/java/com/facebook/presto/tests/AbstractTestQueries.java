bogynick/bogdans

Implement DEALLOCATE PREPARE
Syntax:
DEALLOCATE PREPARE <name>

Confirms existence of prepared statement with name <name> and adds the
statement to a set of deallocated prepared statements.  Sends the set of
deallocated statements back to the client in an HTTP header.  The
client is responsible for maintaining the prepared statements for a
session.

Testing: added unit tests

presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
@@ -3758,6 +3758,7 @@ public abstract class AbstractTestQueries
         assertExplainDdl("ALTER TABLE orders RENAME COLUMN orderkey TO new_column_name");
         assertExplainDdl("SET SESSION foo = 'bar'");
         assertExplainDdl("PREPARE foo FROM SELECT * FROM bar", "PREPARE foo");
+        assertExplainDdl("DEALLOCATE PREPARE foo");
         assertExplainDdl("RESET SESSION foo");
         assertExplainDdl("START TRANSACTION");
         assertExplainDdl("COMMIT");

zz22394/presto

presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueries.java
@@ -4259,6 +4259,7 @@ public abstract class AbstractTestQueries
         assertExplainDdl("ALTER TABLE orders RENAME COLUMN orderkey TO new_column_name");
         assertExplainDdl("SET SESSION foo = 'bar'");
         assertExplainDdl("PREPARE my_query FROM SELECT * FROM orders", "PREPARE my_query");
+        assertExplainDdl("DEALLOCATE PREPARE my_query");
         assertExplainDdl("RESET SESSION foo");
         assertExplainDdl("START TRANSACTION");
         assertExplainDdl("COMMIT");