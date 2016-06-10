7c4f27506c4dec28e0abda9b07bd17293cc29a8b
Maciej Grzybek <maciej.grzybek@teradata.com>
2016-06-10 14:26:44 -0400

    Enable reading and writing FLOAT to/from TextFile in Hive

--- presto-hive/src/main/java/com/facebook/presto/hive/GenericHiveRecordCursor.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/GenericHiveRecordCursor.java
@@ -71,6 +71,7 @@ import static com.facebook.presto.spi.type.Decimals.isLongDecimal;
 import static com.facebook.presto.spi.type.Decimals.isShortDecimal;
 import static com.facebook.presto.spi.type.Decimals.rescale;
 import static com.facebook.presto.spi.type.DoubleType.DOUBLE;
+import static com.facebook.presto.spi.type.FloatType.FLOAT;
 import static com.facebook.presto.spi.type.IntegerType.INTEGER;
 import static com.facebook.presto.spi.type.SmallintType.SMALLINT;
 import static com.facebook.presto.spi.type.TimestampType.TIMESTAMP;
@@ -81,6 +82,7 @@ import static com.facebook.presto.spi.type.Varchars.truncateToLength;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.Maps.uniqueIndex;
+import static java.lang.Float.floatToRawIntBits;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 import static java.lang.String.format;
@@ -354,12 +356,12 @@ class GenericHiveRecordCursor<K, V extends Writable>
         else {
             Object fieldValue = ((PrimitiveObjectInspector) fieldInspectors[column]).getPrimitiveJavaObject(fieldData);
             checkState(fieldValue != null, "fieldValue should not be null");
-            longs[column] = getLongOrTimestamp(fieldValue, hiveStorageTimeZone);
+            longs[column] = getLongExpressedValue(fieldValue, hiveStorageTimeZone);
             nulls[column] = false;
         }
     }
 
-    private static long getLongOrTimestamp(Object value, DateTimeZone hiveTimeZone)
+    private static long getLongExpressedValue(Object value, DateTimeZone hiveTimeZone)
     {
         if (value instanceof Date) {
             long storageTime = ((Date) value).getTime();
@@ -383,6 +385,9 @@ class GenericHiveRecordCursor<K, V extends Writable>
             long utcMillis = hiveTimeZone.convertLocalToUTC(hiveMillis, false);
 
             return utcMillis;
+        }
+        if (value instanceof Float) {
+            return floatToRawIntBits(((Float) value));
         }
         return ((Number) value).longValue();
     }
@@ -568,6 +573,9 @@ class GenericHiveRecordCursor<K, V extends Writable>
         }
         else if (DOUBLE.equals(type)) {
             parseDoubleColumn(column);
+        }
+        else if (FLOAT.equals(type)) {
+            parseLongColumn(column);
         }
         else if (isVarcharType(type) || VARBINARY.equals(type)) {
             parseStringColumn(column);
