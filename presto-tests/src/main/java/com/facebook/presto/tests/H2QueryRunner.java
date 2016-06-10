4a11e88613bda054adc33cd54211b88300860060
Maciej Grzybek <maciej.grzybek@teradata.com>
2016-06-10 09:34:37 -0400

    Enable FLOAT for Hive connector
    * Make buckets, partitions and type utils aware of FLOAT type
    * Support Parquet, ORC, Textfile, RCFILE and RC BINARY file types
    * Update TupleDomainParquetPredicate such that FLOAT predicates
    continue to be pushed down
    * Update TupleDomainOrcPredicate such that FLOAT predicates
    continue to be pushed down. For ORC, both float and double
    statistics are stored in DoubleStatistics
    (https://cwiki.apache.org/confluence/display/Hive/LanguageManual+ORC)

--- presto-tests/src/main/java/com/facebook/presto/tests/H2QueryRunner.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/H2QueryRunner.java
@@ -53,6 +53,7 @@ import static com.facebook.presto.spi.type.BooleanType.BOOLEAN;
 import static com.facebook.presto.spi.type.Chars.isCharType;
 import static com.facebook.presto.spi.type.DateType.DATE;
 import static com.facebook.presto.spi.type.DoubleType.DOUBLE;
+import static com.facebook.presto.spi.type.FloatType.FLOAT;
 import static com.facebook.presto.spi.type.IntegerType.INTEGER;
 import static com.facebook.presto.spi.type.SmallintType.SMALLINT;
 import static com.facebook.presto.spi.type.TimeType.TIME;
@@ -206,6 +207,15 @@ public class H2QueryRunner
                         }
                         else {
                             row.add(doubleValue);
+                        }
+                    }
+                    else if (FLOAT.equals(type)) {
+                        float floatValue = resultSet.getFloat(i);
+                        if (resultSet.wasNull()) {
+                            row.add(null);
+                        }
+                        else {
+                            row.add(floatValue);
                         }
                     }
                     else if (isVarcharType(type)) {
