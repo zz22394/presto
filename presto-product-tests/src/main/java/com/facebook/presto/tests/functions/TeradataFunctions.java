c0f552ff95feede91feca5f7f9956a1ca4fff465
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-06-17 11:19:23 +0200

    Remove QuerygridTestsGroups

--- presto-product-tests/src/main/java/com/facebook/presto/tests/functions/TeradataFunctions.java
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/functions/TeradataFunctions.java
@@ -11,7 +11,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.tests.querygrid;
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.tests.functions;
 
 import com.teradata.tempto.ProductTest;
 import org.testng.annotations.Test;
@@ -19,12 +33,13 @@ import org.testng.annotations.Test;
 import java.sql.Date;
 import java.sql.Timestamp;
 
-import static com.facebook.presto.tests.querygrid.QuerygridTestGroups.FUNCTIONS;
+import static com.facebook.presto.tests.TestGroups.FUNCTIONS;
 import static com.facebook.presto.tests.utils.QueryExecutors.onPresto;
 import static com.teradata.tempto.assertions.QueryAssert.Row.row;
 import static com.teradata.tempto.assertions.QueryAssert.assertThat;
 
-public class ProductTestTeradataFunctions extends ProductTest
+public class TeradataFunctions
+        extends ProductTest
 {
     @Test(groups = FUNCTIONS)
     public void testIndex()
