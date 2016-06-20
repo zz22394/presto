399febcb0b85800cd0b9b7e17c6e0f90fe3efef0
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2016-06-20 10:26:14 +0200

    Separate VARCHAR (unbounded) from VARCHAR(x)
    This separates VARCHAR(x) and VARCHAR from user perspective.
    Max length of VARCHAR(x) available for user is 2147483646 now
    to create unbounded VARCHAR type one should use unparametrized
    type.

--- presto-base-jdbc/src/main/java/com/facebook/presto/plugin/jdbc/BaseJdbcClient.java
+++ presto-base-jdbc/src/main/java/com/facebook/presto/plugin/jdbc/BaseJdbcClient.java
@@ -62,12 +62,12 @@ import static com.facebook.presto.spi.type.TimestampType.TIMESTAMP;
 import static com.facebook.presto.spi.type.TimestampWithTimeZoneType.TIMESTAMP_WITH_TIME_ZONE;
 import static com.facebook.presto.spi.type.TinyintType.TINYINT;
 import static com.facebook.presto.spi.type.VarbinaryType.VARBINARY;
+import static com.facebook.presto.spi.type.VarcharType.createUnboundedVarcharType;
 import static com.facebook.presto.spi.type.VarcharType.createVarcharType;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static com.google.common.collect.Iterables.getOnlyElement;
 import static com.google.common.collect.Maps.fromProperties;
-import static java.lang.Math.min;
 import static java.util.Collections.nCopies;
 import static java.util.Locale.ENGLISH;
 import static java.util.Objects.requireNonNull;
@@ -450,7 +450,10 @@ public class BaseJdbcClient
             case Types.NVARCHAR:
             case Types.LONGVARCHAR:
             case Types.LONGNVARCHAR:
-                return createVarcharType(min(columnSize, VarcharType.MAX_LENGTH));
+                if (columnSize > VarcharType.MAX_LENGTH) {
+                    return createUnboundedVarcharType();
+                }
+                return createVarcharType(columnSize);
             case Types.BINARY:
             case Types.VARBINARY:
             case Types.LONGVARBINARY:
@@ -468,7 +471,7 @@ public class BaseJdbcClient
     protected String toSqlType(Type type)
     {
         if (type instanceof VarcharType) {
-            if (((VarcharType) type).getLength() == VarcharType.MAX_LENGTH) {
+            if (((VarcharType) type).isUnbounded()) {
                 return "varchar";
             }
             return "varchar(" + ((VarcharType) type).getLength() + ")";
--- presto-main/src/test/java/com/facebook/presto/operator/scalar/TestStringFunctions.java
+++ presto-main/src/test/java/com/facebook/presto/operator/scalar/TestStringFunctions.java
@@ -27,7 +27,7 @@ import static com.facebook.presto.spi.StandardErrorCode.INVALID_FUNCTION_ARGUMEN
 import static com.facebook.presto.spi.type.BigintType.BIGINT;
 import static com.facebook.presto.spi.type.VarbinaryType.VARBINARY;
 import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
-import static com.facebook.presto.spi.type.VarcharType.VARCHAR_UNBOUNDED;
+import static com.facebook.presto.spi.type.VarcharType.createUnboundedVarcharType;
 import static com.facebook.presto.spi.type.VarcharType.createVarcharType;
 
 public class TestStringFunctions
@@ -39,7 +39,7 @@ public class TestStringFunctions
     }
 
     @ScalarFunction(value = "utf8", deterministic = false)
-    @SqlType(VARCHAR_UNBOUNDED)
+    @SqlType(StandardTypes.VARCHAR)
     public static Slice convertBinaryToVarchar(@SqlType(StandardTypes.VARBINARY) Slice binary)
     {
         return binary;
@@ -383,7 +383,7 @@ public class TestStringFunctions
     @Test
     public void testVarcharToVarcharX()
     {
-        assertFunction("LOWER(CAST('HELLO' AS VARCHAR))", createVarcharType(Integer.MAX_VALUE), "hello");
+        assertFunction("LOWER(CAST('HELLO' AS VARCHAR))", createUnboundedVarcharType(), "hello");
     }
 
     @Test
