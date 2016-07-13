eb1119f6f15b6223fca402ed8aa651e80009199c
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-07-13 19:28:58 +0200

    Make Spiller Factory pluggable
    Implemented by analogy to NetworkTopology.
    To replace the default SpillerFactory implementation, which is
    BinaryFileSpillerFactory, the `experimental.spiller-implementation`
    property must be set to `custom`, and the custom implementation must be
    provided by the additional module in `PrestoServer#getAdditionalModules`.

--- presto-main/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
+++ presto-main/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
@@ -23,6 +23,7 @@ import java.util.Map;
 import static com.facebook.presto.sql.analyzer.FeaturesConfig.FILE_BASED_RESOURCE_GROUP_MANAGER;
 import static com.facebook.presto.sql.analyzer.FeaturesConfig.ProcessingOptimization.COLUMNAR_DICTIONARY;
 import static com.facebook.presto.sql.analyzer.FeaturesConfig.ProcessingOptimization.DISABLED;
+import static com.facebook.presto.sql.analyzer.FeaturesConfig.SpillerImplementation.BINARY_FILE;
 import static com.facebook.presto.sql.analyzer.RegexLibrary.JONI;
 import static com.facebook.presto.sql.analyzer.RegexLibrary.RE2J;
 import static io.airlift.configuration.testing.ConfigAssertions.assertDeprecatedEquivalence;
@@ -53,7 +54,8 @@ public class TestFeaturesConfig
                 .setRe2JDfaRetries(5)
                 .setResourceGroupManager(FILE_BASED_RESOURCE_GROUP_MANAGER)
                 .setResourceGroupManager(FILE_BASED_RESOURCE_GROUP_MANAGER)
-                .setOperatorMemoryLimitBeforeSpill(DataSize.valueOf("0MB")));
+                .setOperatorMemoryLimitBeforeSpill(DataSize.valueOf("0MB"))
+                .setSpillerImplementation(BINARY_FILE));
     }
 
     @Test
@@ -78,6 +80,7 @@ public class TestFeaturesConfig
                 .put("re2j.dfa-retries", "42")
                 .put("resource-group-manager", "test")
                 .put("experimental.operator-memory-limit-before-spill", "100MB")
+                .put("experimental.spiller-implementation", "custom")
                 .build();
         Map<String, String> properties = new ImmutableMap.Builder<String, String>()
                 .put("experimental-syntax-enabled", "true")
@@ -98,6 +101,7 @@ public class TestFeaturesConfig
                 .put("re2j.dfa-retries", "42")
                 .put("resource-group-manager", "test")
                 .put("experimental.operator-memory-limit-before-spill", "100MB")
+                .put("experimental.spiller-implementation", "custom")
                 .build();
 
         FeaturesConfig expected = new FeaturesConfig()
@@ -119,7 +123,8 @@ public class TestFeaturesConfig
                 .setRe2JDfaRetries(42)
                 .setResourceGroupManager("test")
                 .setResourceGroupManager("test")
-                .setOperatorMemoryLimitBeforeSpill(DataSize.valueOf("100MB"));
+                .setOperatorMemoryLimitBeforeSpill(DataSize.valueOf("100MB"))
+                .setSpillerImplementation("custom");
 
         assertFullMapping(properties, expected);
         assertDeprecatedEquivalence(FeaturesConfig.class, properties, propertiesLegacy);
