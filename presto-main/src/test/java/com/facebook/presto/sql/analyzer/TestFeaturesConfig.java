3a30a7b48cadf7eb3331d4f325062f739bb78872
Artur Gajowy <artur.gajowy@gmail.com>
2016-07-21 10:19:13 +0200

    Add `experimental.spiller-spill-path` property (#264)

--- presto-main/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
+++ presto-main/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
@@ -18,6 +18,7 @@ import io.airlift.configuration.testing.ConfigAssertions;
 import io.airlift.units.DataSize;
 import org.testng.annotations.Test;
 
+import java.nio.file.Paths;
 import java.util.Map;
 
 import static com.facebook.presto.sql.analyzer.FeaturesConfig.FILE_BASED_RESOURCE_GROUP_MANAGER;
@@ -56,7 +57,8 @@ public class TestFeaturesConfig
                 .setCharPadSpaces(false)
                 .setResourceGroupManager(FILE_BASED_RESOURCE_GROUP_MANAGER)
                 .setOperatorMemoryLimitBeforeSpill(DataSize.valueOf("0MB"))
-                .setSpillerImplementation(BINARY_FILE));
+                .setSpillerImplementation(BINARY_FILE)
+                .setSpillerSpillPath(Paths.get(System.getProperty("java.io.tmpdir"), "presto", "spills").toString()));
     }
 
     @Test
@@ -83,6 +85,7 @@ public class TestFeaturesConfig
                 .put("parse-decimal-literals-as-double", "true")
                 .put("experimental.operator-memory-limit-before-spill", "100MB")
                 .put("experimental.spiller-implementation", "custom")
+                .put("experimental.spiller-spill-path", "/tmp/custom/spill/path")
                 .build();
         Map<String, String> properties = new ImmutableMap.Builder<String, String>()
                 .put("experimental-syntax-enabled", "true")
@@ -105,6 +108,7 @@ public class TestFeaturesConfig
                 .put("parse-decimal-literals-as-double", "true")
                 .put("experimental.operator-memory-limit-before-spill", "100MB")
                 .put("experimental.spiller-implementation", "custom")
+                .put("experimental.spiller-spill-path", "/tmp/custom/spill/path")
                 .build();
 
         FeaturesConfig expected = new FeaturesConfig()
@@ -128,7 +132,8 @@ public class TestFeaturesConfig
                 .setCharPadSpaces(true)
                 .setResourceGroupManager("test")
                 .setOperatorMemoryLimitBeforeSpill(DataSize.valueOf("100MB"))
-                .setSpillerImplementation("custom");
+                .setSpillerImplementation("custom")
+                .setSpillerSpillPath("/tmp/custom/spill/path");
 
         assertFullMapping(properties, expected);
         assertDeprecatedEquivalence(FeaturesConfig.class, properties, propertiesLegacy);
