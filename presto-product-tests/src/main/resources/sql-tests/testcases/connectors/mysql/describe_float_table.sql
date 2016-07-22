f4b3fdf5b1dd1249eef69028c4453270a2061a51
Akshat Nair <akshat.nair@teradata.com>
2016-07-21 21:57:38 -0400

    Float support in JDBC connectors

---
+++ presto-product-tests/src/main/resources/sql-tests/testcases/connectors/mysql/describe_float_table.sql
@@ -0,0 +1,10 @@
+-- database: presto; groups: mysql_connector; queryType: SELECT; tables: mysql.float_table_mysql
+--!
+describe mysql.test.float_table_mysql
+--!
+-- delimiter: |; trimValues: true; ignoreOrder: true;
+id_employee        | integer     | |
+salary             | double      | |
+bonus              | float       | |
+tip                | float       | |
+tip2               | double      | |
