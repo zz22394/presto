4f3cebe284c4597ce7b2e5fc1df8f5a313ea25ea
Maciej Grzybek <maciej.grzybek@teradata.com>
2016-07-25 16:44:11 -0400

    Introduce Frame-per-Function mapping in WindowNode
    Introduced WindowAndFrame helper structure to make serialization easier.
    This was needed because JSON does not accept object as a kep in map, thus
    Map<non-string-object, _> wouldn't work.
    
    Internally frames are stored as a map instead of serializable List
    because Map features are needed more often (while planning) than (de)serialization happens.

--- presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/MergeIdenticalWindows.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/MergeIdenticalWindows.java
@@ -21,12 +21,12 @@ import com.facebook.presto.sql.planner.SymbolAllocator;
 import com.facebook.presto.sql.planner.plan.PlanNode;
 import com.facebook.presto.sql.planner.plan.SimplePlanRewriter;
 import com.facebook.presto.sql.planner.plan.WindowNode;
-import com.facebook.presto.sql.planner.plan.WindowNode.Specification;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.Multimap;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.checkState;
@@ -77,13 +77,13 @@ public class MergeIdenticalWindows
     }
 
     private static class Rewriter
-            extends SimplePlanRewriter<Multimap<Specification, WindowNode>>
+            extends SimplePlanRewriter<Multimap<Rewriter.SpecificationAndFrame, WindowNode>>
     {
         @Override
-        protected PlanNode visitPlan(PlanNode node, RewriteContext<Multimap<Specification, WindowNode>> context)
+        protected PlanNode visitPlan(PlanNode node, RewriteContext<Multimap<SpecificationAndFrame, WindowNode>> context)
         {
             PlanNode newNode = context.defaultRewrite(node, ImmutableListMultimap.of());
-            for (Specification specification : context.get().keySet()) {
+            for (SpecificationAndFrame specification : context.get().keySet()) {
                 Collection<WindowNode> windows = context.get().get(specification);
                 newNode = collapseWindows(newNode, specification, windows);
             }
@@ -93,33 +93,77 @@ public class MergeIdenticalWindows
         @Override
         public PlanNode visitWindow(
                 WindowNode node,
-                RewriteContext<Multimap<Specification, WindowNode>> context)
+                RewriteContext<Multimap<SpecificationAndFrame, WindowNode>> context)
         {
             checkState(!node.getHashSymbol().isPresent(), "MergeIdenticalWindows should be run before HashGenerationOptimizer");
             checkState(node.getPrePartitionedInputs().isEmpty() && node.getPreSortedOrderPrefix() == 0, "MergeIdenticalWindows should be run before AddExchanges");
+            checkState(node.getFrames().values().size() == 1, "More than one frame per WindowNode is not yet supported");
 
             return context.rewrite(
                     node.getSource(),
-                    ImmutableListMultimap.<Specification, WindowNode>builder()
-                            .put(node.getSpecification(), node) // Add the current window first so that it gets precedence in iteration order
+                    ImmutableListMultimap.<SpecificationAndFrame, WindowNode>builder()
+                            .put(new SpecificationAndFrame(node), node) // Add the current window first so that it gets precedence in iteration order
                             .putAll(context.get())
                             .build());
         }
 
-        private static WindowNode collapseWindows(PlanNode source, Specification specification, Collection<WindowNode> windows)
+        private static WindowNode collapseWindows(PlanNode source, SpecificationAndFrame specification, Collection<WindowNode> windows)
         {
             WindowNode canonical = windows.iterator().next();
+            WindowNode.Frame frame = canonical.getFrames().values().iterator().next(); // asserted in #visitWindow already
             return new WindowNode(
                     canonical.getId(),
                     source,
-                    specification,
+                    specification.specification,
                     windows.stream()
                             .map(WindowNode::getWindowFunctions)
                             .flatMap(map -> map.entrySet().stream())
                             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)),
+                    windows.stream()
+                            .map(WindowNode::getWindowFunctions)
+                            .flatMap(map -> map.entrySet().stream())
+                            .collect(Collectors.toMap(Map.Entry::getValue, ignore -> frame)),
                     canonical.getHashSymbol(),
                     canonical.getPrePartitionedInputs(),
                     canonical.getPreSortedOrderPrefix());
+        }
+
+        static final class SpecificationAndFrame
+        {
+            private final WindowNode.Specification specification;
+            private final WindowNode.Frame frame;
+
+            SpecificationAndFrame(WindowNode.Specification specification, WindowNode.Frame frame)
+            {
+                this.specification = specification;
+                this.frame = frame;
+            }
+
+            SpecificationAndFrame(WindowNode node)
+            {
+                this(node.getSpecification(),
+                        node.getFrames().values().iterator().next()); // asserted in #visitWindow already
+            }
+
+            @Override
+            public int hashCode()
+            {
+                return Objects.hash(specification, frame);
+            }
+
+            @Override
+            public boolean equals(Object obj)
+            {
+                if (this == obj) {
+                    return true;
+                }
+                if (obj == null || getClass() != obj.getClass()) {
+                    return false;
+                }
+                SpecificationAndFrame other = (SpecificationAndFrame) obj;
+                return Objects.equals(this.specification, other.specification) &&
+                        Objects.equals(this.frame, other.frame);
+            }
         }
     }
 }
