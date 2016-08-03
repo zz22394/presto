db7c23b941406960622ee513989ae606ae548301
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2016-08-03 14:47:01 +0200

    Introduce CLI custom message for 404 http status error

--- presto-cli/src/main/java/com/facebook/presto/cli/ErrorMessages.java
+++ presto-cli/src/main/java/com/facebook/presto/cli/ErrorMessages.java
@@ -15,6 +15,7 @@ package com.facebook.presto.cli;
 
 import com.facebook.presto.client.ClientSession;
 import com.facebook.presto.client.PrestoClientException;
+import io.airlift.http.client.HttpStatus;
 
 import java.io.EOFException;
 import java.net.ConnectException;
@@ -26,6 +27,7 @@ import static java.lang.String.format;
 public class ErrorMessages
 {
     private static final String PRESTO_COORDINATOR_NOT_FOUND = "There was a problem with a response from Presto Coordinator.\n";
+    private static final String PRESTO_COORDINATOR_404 = "Presto HTTP interface returned 404 (file not found).\n";
 
     private static final String TECHNICAL_DETAILS_HEADER = "\n=========   TECHNICAL DETAILS   =========\n";
     private static final String SESSION_INTRO = "[ Session information ]\n";
@@ -38,7 +40,9 @@ public class ErrorMessages
         VERIFY_PRESTO_RUNNING("Verify that Presto is running on %s."),
         DEFINE_SERVER_AS_CLI_PARAM("Use '--server' argument when starting Presto CLI to define server host and port."),
         CHECK_NETWORK("Check the network conditions between client and server."),
-        USE_DEBUG_MODE("Use '--debug' argument to get more technical details.");
+        USE_DEBUG_MODE("Use '--debug' argument to get more technical details."),
+        CHECK_OTHER_HTTP_SERVICE("Make sure that none other HTTP service is running on %s."),
+        CLIENT_IS_UP_TO_DATE_WITH_SERVER("Update CLI to match Presto server version.");
 
         private static final String TIPS_INTRO = "To solve this problem you may try to:\n";
 
@@ -98,8 +102,16 @@ public class ErrorMessages
     private static String prestoClientExceptionErrorMesage(PrestoClientException clientException, ClientSession session)
     {
         StringBuilder builder = new StringBuilder();
+        boolean wasErrorIdentified = false;
 
-        builder.append(clientException.getMessage());
+        if (clientException.getResponse().getStatusCode() == HttpStatus.NOT_FOUND.code()) {
+            serverFileNotFoundErrorMessage(builder, session);
+            wasErrorIdentified = true;
+        }
+
+        if (!wasErrorIdentified) {
+            builder.append(clientException.getMessage());
+        }
 
         if (session.isDebug()) {
             technicalDetailsPrestoServerExceptionErrorMessage(builder, clientException, session);
@@ -149,6 +161,21 @@ public class ErrorMessages
         tipsBuilder.addTip(Tip.VERIFY_PRESTO_RUNNING, session.getServer())
                 .addTip(Tip.DEFINE_SERVER_AS_CLI_PARAM)
                 .addTip(Tip.CHECK_NETWORK).build();
+        if (!session.isDebug()) {
+            tipsBuilder.addTip(Tip.USE_DEBUG_MODE);
+        }
+        builder.append(tipsBuilder.build());
+    }
+
+    private static void serverFileNotFoundErrorMessage(StringBuilder builder, ClientSession session)
+    {
+        builder.append(PRESTO_COORDINATOR_404);
+        Tip.Builder tipsBuilder = Tip.builder();
+        tipsBuilder.addTip(Tip.VERIFY_PRESTO_RUNNING, session.getServer())
+                .addTip(Tip.DEFINE_SERVER_AS_CLI_PARAM)
+                .addTip(Tip.CHECK_NETWORK)
+                .addTip(Tip.CHECK_OTHER_HTTP_SERVICE, session.getServer())
+                .addTip(Tip.CLIENT_IS_UP_TO_DATE_WITH_SERVER);
         if (!session.isDebug()) {
             tipsBuilder.addTip(Tip.USE_DEBUG_MODE);
         }
