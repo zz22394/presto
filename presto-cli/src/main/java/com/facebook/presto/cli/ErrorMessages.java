203580ede0217c335d77bcfac4bd22127ae44fa2
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2016-08-03 14:20:26 +0200

    Introduce CLI custom message for server not found error
    This commits also introduce Tips enum which will be used in further
    error handling allowing to avoid copy pasting as well as extracting
    logic of formating user Tips.

--- presto-cli/src/main/java/com/facebook/presto/cli/ErrorMessages.java
+++ presto-cli/src/main/java/com/facebook/presto/cli/ErrorMessages.java
@@ -16,10 +16,17 @@ package com.facebook.presto.cli;
 import com.facebook.presto.client.ClientSession;
 import com.facebook.presto.client.PrestoClientException;
 
+import java.io.EOFException;
+import java.net.ConnectException;
 import java.util.Arrays;
+
+import static com.google.common.base.Throwables.getCausalChain;
+import static java.lang.String.format;
 
 public class ErrorMessages
 {
+    private static final String PRESTO_COORDINATOR_NOT_FOUND = "There was a problem with a response from Presto Coordinator.\n";
+
     private static final String TECHNICAL_DETAILS_HEADER = "\n=========   TECHNICAL DETAILS   =========\n";
     private static final String SESSION_INTRO = "[ Session information ]\n";
     private static final String ERROR_MESSAGE_INTRO = "[ Error message ]\n";
@@ -27,6 +34,53 @@ public class ErrorMessages
     private static final String RESPONSE_INTRO = "[ Plain text HTTP response ]\n";
     private static final String TECHNICAL_DETAILS_END = "========= TECHNICAL DETAILS END =========\n\n";
 
+    private enum Tips {
+        VERIFY_PRESTO_RUNNING("Verify that Presto is running on %s."),
+        DEFINE_SERVER_AS_CLI_PARAM("Use '--server' argument when starting Presto CLI to define server host and port."),
+        CHECK_NETWORK("Check the network conditions between client and server."),
+        USE_DEBUG_MODE("Use '--debug' argument to get more technical details.");
+
+        private static final String TIPS_INTRO = "To solve this problem you may try to:\n";
+
+        private final String message;
+
+        private Tips(String message)
+        {
+            this.message = message;
+        }
+
+        @Override
+        public String toString()
+        {
+            return message;
+        }
+
+        public static Builder builder()
+        {
+            return new Builder();
+        }
+
+        private static class Builder
+        {
+            private StringBuilder builder = new StringBuilder();
+
+            public Builder()
+            {
+                builder.append(TIPS_INTRO);
+            }
+
+            public Builder addTip(Tips tip, Object ...toFormat)
+            {
+                builder.append(format(" * " + tip.toString() + "\n", toFormat));
+                return this;
+            }
+
+            public String build()
+            {
+                return builder.toString();
+            }
+        }
+    }
 
     public static String createErrorMessage(Throwable throwable, ClientSession session)
     {
@@ -56,8 +110,13 @@ public class ErrorMessages
     {
         StringBuilder builder = new StringBuilder();
 
-        // We have no clue about what went wrong, just display what we obtained.
-        builder.append("Error running command:\n" + throwable.getMessage() + "\n");
+        if (getCausalChain(throwable).stream().anyMatch(x -> x instanceof EOFException || x instanceof ConnectException)) {
+            serverNotFoundErrorMessage(builder, session);
+        }
+        else {
+            // We have no clue about what went wrong, just display what we obtained.
+            builder.append("Error running command:\n" + throwable.getMessage() + "\n");
+        }
 
         if (session.isDebug()) {
             technicalDetailsRuntimeExceptionErrorMessage(builder, throwable, session);
@@ -72,6 +131,19 @@ public class ErrorMessages
     }
 
     //region Messages for given problems
+    private static void serverNotFoundErrorMessage(StringBuilder builder, ClientSession session)
+    {
+        builder.append(PRESTO_COORDINATOR_NOT_FOUND);
+        Tips.Builder tipsBuilder = Tips.builder();
+        tipsBuilder.addTip(Tips.VERIFY_PRESTO_RUNNING, session.getServer())
+                .addTip(Tips.DEFINE_SERVER_AS_CLI_PARAM)
+                .addTip(Tips.CHECK_NETWORK).build();
+        if (!session.isDebug()) {
+            tipsBuilder.addTip(Tips.USE_DEBUG_MODE);
+        }
+        builder.append(tipsBuilder.build());
+    }
+
     private static void technicalDetailsPrestoServerExceptionErrorMessage(StringBuilder builder, PrestoClientException serverException, ClientSession session)
     {
         builder.append(TECHNICAL_DETAILS_HEADER);
