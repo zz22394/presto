44340106a96c3a581094c1de386ae5b9f9ba07cb
Christina Wallin <christina.wallin@teradata.com>
2016-08-05 16:46:57 -0400

    Modify CLI to support long prepared statements
    In order to support >4k prepared statements, send the prepared
    statements along with the query via the Jackson-serialized object
    QuerySubmission.

--- presto-cli/src/main/java/com/facebook/presto/cli/QueryRunner.java
+++ presto-cli/src/main/java/com/facebook/presto/cli/QueryRunner.java
@@ -15,6 +15,7 @@ package com.facebook.presto.cli;
 
 import com.facebook.presto.client.ClientSession;
 import com.facebook.presto.client.QueryResults;
+import com.facebook.presto.client.QuerySubmission;
 import com.facebook.presto.client.StatementClient;
 import com.google.common.collect.ImmutableList;
 import com.google.common.net.HostAndPort;
@@ -41,10 +42,12 @@ public class QueryRunner
     private final JsonCodec<QueryResults> queryResultsCodec;
     private final AtomicReference<ClientSession> session;
     private final HttpClient httpClient;
+    private final JsonCodec<QuerySubmission> querySubmissionCodec;
 
     public QueryRunner(
             ClientSession session,
             JsonCodec<QueryResults> queryResultsCodec,
+            JsonCodec<QuerySubmission> querySubmissionCodec,
             Optional<HostAndPort> socksProxy,
             Optional<String> keystorePath,
             Optional<String> keystorePassword,
@@ -55,6 +58,7 @@ public class QueryRunner
     {
         this.session = new AtomicReference<>(requireNonNull(session, "session is null"));
         this.queryResultsCodec = requireNonNull(queryResultsCodec, "queryResultsCodec is null");
+        this.querySubmissionCodec = requireNonNull(querySubmissionCodec, "querySubmissionCodec is null");
         this.httpClient = new JettyHttpClient(
                 getHttpClientConfig(socksProxy, keystorePath, keystorePassword, kerberosPrincipal, kerberosRemoteServiceName, authenticationEnabled),
                 kerberosConfig,
@@ -79,7 +83,7 @@ public class QueryRunner
 
     public StatementClient startInternalQuery(String query)
     {
-        return new StatementClient(httpClient, queryResultsCodec, session.get(), query);
+        return new StatementClient(httpClient, queryResultsCodec, querySubmissionCodec, session.get(), query);
     }
 
     @Override
@@ -101,6 +105,7 @@ public class QueryRunner
         return new QueryRunner(
                 session,
                 jsonCodec(QueryResults.class),
+                jsonCodec(QuerySubmission.class),
                 socksProxy,
                 keystorePath,
                 keystorePassword,
