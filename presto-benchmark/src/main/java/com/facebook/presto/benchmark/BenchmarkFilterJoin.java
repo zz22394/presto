d46e3333a48aa2039c0872d069d63412a26de4e0
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-08-08 14:40:11 +0200

    Benchmark for join and filter

---
+++ presto-benchmark/src/main/java/com/facebook/presto/benchmark/BenchmarkFilterJoin.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.benchmark;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.Warmup;
+
+@Fork(1)
+@Warmup(iterations = 30)
+@Measurement(iterations = 30)
+public class BenchmarkFilterJoin
+        extends AbstractJmhSqlBenchmark
+{
+    public static class AggregationContext
+            extends AbstractContext
+    {
+        @Param({"SELECT * FROM t t1 JOIN t t2 ON t1.a = t2.a WHERE (t1.b+t2.b)*5 > 100000000"})
+        protected String sql;
+
+        @Setup
+        public void setUp()
+        {
+            runQuery("CREATE TABLE t AS SELECT orderkey % 1000 as a, orderkey % 1000 as b, comment as c1, comment as c2, comment as c3, comment as c4 FROM tpch.sf1.orders LIMIT 10000");
+        }
+
+        public void run()
+        {
+            runQuery(sql);
+        }
+    }
+
+    @Benchmark
+    public void benchmarkJoinAndFilter(AggregationContext context)
+    {
+        context.run();
+    }
+
+    public static void main(String[] args)
+    {
+        AggregationContext c = new AggregationContext();
+        c.setUp();
+        c.run();
+    }
+}
