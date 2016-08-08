282370bd930cd1b3b81798626309c1e2a6c40c68
Amruta Gokhale <amruta.gokhale@teradata.com>
2016-08-08 13:47:44 -0400

    Add OpenLDAP object definitions and requirements

---
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/ImmutableLdapObjectDefinitions.java
@@ -0,0 +1,139 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.tests;
+
+import com.google.common.collect.ImmutableMap;
+import com.teradata.tempto.fulfillment.ldap.LdapObjectDefinition;
+
+import java.util.Arrays;
+
+public final class ImmutableLdapObjectDefinitions
+{
+    private ImmutableLdapObjectDefinitions() {}
+
+    public static final LdapObjectDefinition AMERICA_ORG =
+            LdapObjectDefinition.builder("AmericaOrg")
+                    .setDistinguishedName("ou=America,dc=presto,dc=testldap,dc=com")
+                    .setAttributes(ImmutableMap.of(
+                            "ou", "America",
+                            "name", "America"))
+                    .addObjectClasses(Arrays.asList("top", "organizationalUnit"))
+                    .build();
+
+    public static final LdapObjectDefinition ASIA_ORG =
+            LdapObjectDefinition.builder("AsiaOrg")
+                    .setDistinguishedName("ou=Asia,dc=presto,dc=testldap,dc=com")
+                    .setAttributes(ImmutableMap.of(
+                            "ou", "Asia",
+                            "name", "Asia"))
+                    .addObjectClasses(Arrays.asList("top", "organizationalUnit"))
+                    .build();
+
+    public static final LdapObjectDefinition DEFAULT_GROUP =
+            LdapObjectDefinition.builder("DefaultGroup")
+                    .setDistinguishedName("cn=DefaultGroup,ou=America,dc=presto,dc=testldap,dc=com")
+                    .setAttributes(ImmutableMap.of(
+                            "cn", "DefaultGroup",
+                            "member", "uid=DefaultGroupUser,ou=Asia,dc=presto,dc=testldap,dc=com"))
+                    .addObjectClasses(Arrays.asList("groupOfNames"))
+                    .build();
+
+    public static final LdapObjectDefinition PARENT_GROUP =
+            LdapObjectDefinition.builder("ParentGroup")
+                    .setDistinguishedName("cn=ParentGroup,ou=America,dc=presto,dc=testldap,dc=com")
+                    .setAttributes(ImmutableMap.of(
+                            "cn", "ParentGroup",
+                            "member", "uid=ParentGroupUser,ou=Asia,dc=presto,dc=testldap,dc=com",
+                            "member", "uid=UserInMultipleGroups,ou=Asia,dc=presto,dc=testldap,dc=com"))
+                    .addObjectClasses(Arrays.asList("groupOfNames"))
+                    .build();
+
+    public static final LdapObjectDefinition CHILD_GROUP =
+            LdapObjectDefinition.builder("ChildGroup")
+                    .setDistinguishedName("cn=ChildGroup,ou=America,dc=presto,dc=testldap,dc=com")
+                    .setAttributes(ImmutableMap.of(
+                            "cn", "ChildGroup",
+                            "member", "uid=ChildGroupUser,ou=Asia,dc=presto,dc=testldap,dc=com"))
+                    .addObjectClasses(Arrays.asList("groupOfNames"))
+                    .build();
+
+    public static final LdapObjectDefinition ANOTHER_GROUP =
+            LdapObjectDefinition.builder("AnotherGroup")
+                    .setDistinguishedName("cn=AnotherGroup,ou=America,dc=presto,dc=testldap,dc=com")
+                    .setAttributes(ImmutableMap.of(
+                            "cn", "AnotherGroup",
+                            "member", "uid=UserInMultipleGroups,ou=Asia,dc=presto,dc=testldap,dc=com"))
+                    .addObjectClasses(Arrays.asList("groupOfNames"))
+                    .build();
+
+    public static final LdapObjectDefinition DEFAULT_GROUP_USER =
+            LdapObjectDefinition.builder("DefaultGroupUser")
+                    .setDistinguishedName("uid=defaultgroupuser,ou=Asia,dc=presto,dc=testldap,dc=com")
+                    .setAttributes(ImmutableMap.of(
+                            "cn", "DefaultGroupUser",
+                            "sn", "DefaultGroupUser",
+                            "password", "LDAPPass123",
+                            "memberOf", "DefaultGroup"
+                    ))
+                    .addObjectClasses(Arrays.asList("person", "inetOrgPerson"))
+                    .build();
+
+    public static final LdapObjectDefinition PARENT_GROUP_USER =
+            LdapObjectDefinition.builder("ParentGroupUser")
+                    .setDistinguishedName("uid=parentgroupuser,ou=Asia,dc=presto,dc=testldap,dc=com")
+                    .setAttributes(ImmutableMap.of(
+                            "cn", "ParentGroupUser",
+                            "sn", "ParentGroupUser",
+                            "password", "LDAPPass123",
+                            "memberOf", "ParentGroup"
+                    ))
+                    .addObjectClasses(Arrays.asList("person", "inetOrgPerson"))
+                    .build();
+
+    public static final LdapObjectDefinition CHILD_GROUP_USER =
+            LdapObjectDefinition.builder("ChildGroupUser")
+                    .setDistinguishedName("uid=childgroupuser,ou=Asia,dc=presto,dc=testldap,dc=com")
+                    .setAttributes(ImmutableMap.of(
+                            "cn", "ChildGroupUser",
+                            "sn", "ChildGroupUser",
+                            "password", "LDAPPass123",
+                            "memberOf", "ChildGroup"
+                    ))
+                    .addObjectClasses(Arrays.asList("person", "inetOrgPerson"))
+                    .build();
+
+    public static final LdapObjectDefinition USER_IN_MULTIPLE_GROUPS =
+            LdapObjectDefinition.builder("UserInMultipleGroups")
+                    .setDistinguishedName("uid=userinmultiplegroups,ou=Asia,dc=presto,dc=testldap,dc=com")
+                    .setAttributes(ImmutableMap.of(
+                            "cn", "UserInMultipleGroups",
+                            "sn", "UserInMultipleGroups",
+                            "password", "LDAPPass123",
+                            "memberOf", "DefaultGroup",
+                            "memberOf", "AnotherGroup"
+                    ))
+                    .addObjectClasses(Arrays.asList("person", "inetOrgPerson"))
+                    .build();
+
+    public static final LdapObjectDefinition ORPHAN_USER =
+            LdapObjectDefinition.builder("OrphanUser")
+                    .setDistinguishedName("uid=orphanuser,ou=Asia,dc=presto,dc=testldap,dc=com")
+                    .setAttributes(ImmutableMap.of(
+                            "cn", "OrphanUser",
+                            "sn", "OrphanUser",
+                            "password", "LDAPPass123"
+                    ))
+                    .addObjectClasses(Arrays.asList("person", "inetOrgPerson"))
+                    .build();
+}
---
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/ImmutableLdapObjectRequirements.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.tests;
+
+import com.teradata.tempto.Requirement;
+import com.teradata.tempto.RequirementsProvider;
+import com.teradata.tempto.configuration.Configuration;
+import com.teradata.tempto.fulfillment.ldap.LdapObjectRequirement;
+
+import java.util.Arrays;
+
+import static com.facebook.presto.tests.ImmutableLdapObjectDefinitions.AMERICA_ORG;
+import static com.facebook.presto.tests.ImmutableLdapObjectDefinitions.ANOTHER_GROUP;
+import static com.facebook.presto.tests.ImmutableLdapObjectDefinitions.ASIA_ORG;
+import static com.facebook.presto.tests.ImmutableLdapObjectDefinitions.CHILD_GROUP;
+import static com.facebook.presto.tests.ImmutableLdapObjectDefinitions.CHILD_GROUP_USER;
+import static com.facebook.presto.tests.ImmutableLdapObjectDefinitions.DEFAULT_GROUP;
+import static com.facebook.presto.tests.ImmutableLdapObjectDefinitions.DEFAULT_GROUP_USER;
+import static com.facebook.presto.tests.ImmutableLdapObjectDefinitions.ORPHAN_USER;
+import static com.facebook.presto.tests.ImmutableLdapObjectDefinitions.PARENT_GROUP;
+import static com.facebook.presto.tests.ImmutableLdapObjectDefinitions.PARENT_GROUP_USER;
+import static com.facebook.presto.tests.ImmutableLdapObjectDefinitions.USER_IN_MULTIPLE_GROUPS;
+import static com.teradata.tempto.Requirements.compose;
+
+public class ImmutableLdapObjectRequirements
+        implements RequirementsProvider
+{
+    public static final Requirement DEFAULT_OBJECTS = new LdapObjectRequirement(Arrays.asList(AMERICA_ORG, ASIA_ORG, DEFAULT_GROUP, DEFAULT_GROUP_USER));
+    public static final Requirement PARENT_OBJECTS = new LdapObjectRequirement(Arrays.asList(AMERICA_ORG, ASIA_ORG, PARENT_GROUP, PARENT_GROUP_USER));
+    public static final Requirement CHILD_OBJECTS = new LdapObjectRequirement(Arrays.asList(AMERICA_ORG, ASIA_ORG, CHILD_GROUP, CHILD_GROUP_USER));
+    public static final Requirement MULTIPLE_GROUPS_OBJECTS = new LdapObjectRequirement(Arrays.asList(AMERICA_ORG, ASIA_ORG, DEFAULT_GROUP, ANOTHER_GROUP, USER_IN_MULTIPLE_GROUPS));
+    public static final Requirement ORPHAN_OBJECTS = new LdapObjectRequirement(Arrays.asList(AMERICA_ORG, ASIA_ORG, ORPHAN_USER));
+
+    @Override
+    public Requirement getRequirements(Configuration configuration)
+    {
+        return compose(
+                DEFAULT_OBJECTS,
+                PARENT_OBJECTS,
+                CHILD_OBJECTS,
+                MULTIPLE_GROUPS_OBJECTS,
+                ORPHAN_OBJECTS
+        );
+    }
+
+    public static class ImmutableDefaultUserObject
+            implements RequirementsProvider
+    {
+        @Override
+        public Requirement getRequirements(Configuration configuration)
+        {
+            return DEFAULT_OBJECTS;
+        }
+    }
+
+    public static class ImmutableParentUserObject
+            implements RequirementsProvider
+    {
+        @Override
+        public Requirement getRequirements(Configuration configuration)
+        {
+            return PARENT_OBJECTS;
+        }
+    }
+
+    public static class ImmutableChildUserObject
+            implements RequirementsProvider
+    {
+        @Override
+        public Requirement getRequirements(Configuration configuration)
+        {
+            return CHILD_OBJECTS;
+        }
+    }
+
+    public static class ImmutableUserInMultipleGroupsObject
+            implements RequirementsProvider
+    {
+        @Override
+        public Requirement getRequirements(Configuration configuration)
+        {
+            return MULTIPLE_GROUPS_OBJECTS;
+        }
+    }
+
+    public static class ImmutableOrphanUserObject
+            implements RequirementsProvider
+    {
+        @Override
+        public Requirement getRequirements(Configuration configuration)
+        {
+            return ORPHAN_OBJECTS;
+        }
+    }
+}
